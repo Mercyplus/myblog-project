54da361c523fa3032b688d0fec2fff2f71a52dd3
Mercyplus <sobachka1966@mail.ru>
2021-06-02 00:48:18 +0300

    add models for blog and events

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    image = models.ImageField(upload_to='event_images/')
+    date = models.DateTimeField()
+
+    def get_summary(self):
+        return self.text[:15]
+
+    def __str__(self):
+        return self.title
